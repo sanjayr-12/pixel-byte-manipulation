49ed55c06d9b118e1368053cca9831e1e588b5aa
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-10-10 18:39:36 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -3,17 +3,18 @@ import dotenv from "dotenv";
 import multer from "multer";
 import cors from "cors";
 import { convert } from "./sharp.js";
+import * as fs from "fs";
 
 const app = express();
 dotenv.config();
-app.use(cors({origin:"http://localhost:5173"}));
+app.use(cors({ origin: "http://localhost:5173" }));
 const storage = multer.memoryStorage();
 const upload = multer();
 
 app.post("/img", upload.single("img"), async (req, res) => {
-  const arr = req.file.buffer
-  await convert(arr)
-  res.send("successfull")
+  const arr = req.file.buffer;
+  await convert(arr);
+  res.send("successfull");
 });
 
 app.listen(process.env.PORT, () => {
--- server/sharp.js
+++ server/sharp.js
@@ -3,21 +3,19 @@ import sharp from "sharp";
 export async function convert(buffer) {
   try {
     const data = await sharp(buffer).metadata();
-    //raw data
     const rawBytes = await sharp(buffer)
       .raw()
       .toBuffer({ resolveWithObject: true });
-
-    // let twoD = [];
-    // let ans = convertto2d(twoD, data.width, data.height, rawBytes.data);
+    console.log(rawBytes.data.length);
+    console.log(rawBytes.data);
 
     const div = rawBytes.data.length / 2;
     for (let i = 0; i < rawBytes.data.length; i += 4) {
       if (i <= div) {
-        rawBytes.data[i] = 145;
-        rawBytes.data[i + 1] = 26;
-        rawBytes.data[i + 2] = 200;
-        rawBytes.data[i + 3] = Math.floor(0.9 * 255);
+        rawBytes.data[i] = 240;
+        rawBytes.data[i + 1] = 255;
+        rawBytes.data[i + 2] = 21;
+        rawBytes.data[i + 3] = Math.floor(0.8 * 255);
       } else {
         rawBytes.data[i] = 84;
         rawBytes.data[i + 1] = 135;
@@ -39,43 +37,3 @@ export async function convert(buffer) {
     console.log(error.message);
   }
 }
-
-// const convertto2d = (arr, width, height, originalArr) => {
-//   let start = 0;
-//   let k = 0;
-//   for (let i = 0; i < height; i++) {
-//     start += width;
-//     let row = [];
-//     for (let j = k; j < start; j++) {
-//       row.push(originalArr[j]);
-//     }
-//     k = start;
-//     arr.push(row);
-//   }
-//   //check if the length is same
-//   console.log(arr.length * arr[0].length * 4 === width * height * 4);
-
-//   let ans = [];
-
-//   for (let i = 0; i < height / 2; i++) {
-//     for (let j = 0; j < width; j += 4) {
-//       ans.push((arr[i][j] = 176));
-//       ans.push((arr[i][j + 1] = 172));
-//       ans.push((arr[i][j + 2] = 21));
-//       ans.push((arr[i][j + 3] = Math.floor(0.8 * 255)));
-//     }
-//   }
-//   for (let i = height / 2; i < height; i++) {
-//     for (let j = 0; j < width; j += 4) {
-//       ans.push((arr[i][j] = 145));
-//       ans.push((arr[i][j + 1] = 26));
-//       ans.push((arr[i][j + 2] = 200));
-//       ans.push((arr[i][j + 3] = Math.floor(0.08 * 255)));
-//     }
-//   }
-//   console.log(ans.length);
-
-//   console.log(ans.length === height * width * 4);
-
-//   return ans;
-// };
